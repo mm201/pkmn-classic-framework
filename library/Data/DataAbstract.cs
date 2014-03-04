a57eb7c399d376ab3b9b29089094d4ba95aadec6
Greg Edwards <edwardsgreg@gmail.com>
2014-03-03 18:19:21 -0800

    Remove veekun abstract methods from my main data classes.

--- library/Data/DataAbstract.cs
+++ library/Data/DataAbstract.cs
@@ -6,7 +6,7 @@ using System.Data;
 
 namespace PokeFoundations.Data
 {
-    abstract class DataAbstract
+    public abstract class DataAbstract
     {
         public static String SqlSanitize(String s)
         {
@@ -20,37 +20,5 @@ namespace PokeFoundations.Data
             if (x != -1) result = result.Substring(0, x);
             return result;
         }
-
-        #region Veekun conversions
-        /// <summary>
-        /// Loads the PF database with abilities from Veekun.
-        /// </summary>
-        public abstract void AbilitiesFromVeekun(DataTable abilities);
-
-        /// <summary>
-        /// Loads the PF database with all items, berries, and TMs from Veekun.
-        /// </summary>
-        public abstract void ItemsFromVeekun(DataTable items, DataTable berries, DataTable machines);
-
-        /// <summary>
-        /// Loads the PF database with moves from Veekun.
-        /// </summary>
-        public abstract void MovesFromVeekun(DataTable moves);
-
-        /// <summary>
-        /// Loads the PF database with Pokémon from Veekun.
-        /// </summary>
-        public abstract void PokemonFromVeekun(DataTable pokemon);
-        #endregion
-
-        #region Veekun retrieval
-        public abstract DataTable GetVeekunAbilities();
-        public abstract DataTable GetVeekunItems();
-        public abstract DataTable GetVeekunBerries();
-        public abstract DataTable GetVeekunBerryFlavours();
-        public abstract DataTable GetVeekunMachines();
-        public abstract DataTable GetVeekunMoves();
-        public abstract DataTable GetVeekunPokemon();
-        #endregion
     }
 }
