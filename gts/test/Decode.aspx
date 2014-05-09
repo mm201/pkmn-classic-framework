9c68667bb8b75a39795fba1165f0abaf9763decc
Greg Edwards <edwardsgreg@gmail.com>
2014-05-08 21:12:40 -0400

    Added back data request decoder.

---
+++ gts/test/Decode.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeBehind="Decode.aspx.cs" Inherits="PkmnFoundations.GTS.debug.Decode" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="cpHead" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="cpMain" runat="server">
+<form id="theForm" runat="server">
+    <div>
+    <p>Enter data querystring to decode:</p>
+    <asp:TextBox ID="txtData" Width="600" runat="server" />
+    <asp:Button ID="btnDecode" Text="Decode" OnClick="btnDecode_Click" runat="server" />
+    </div>
+    <asp:Literal ID="litMessage" runat="server" />
+    <asp:PlaceHolder ID="phDecoded" Visible="false" runat="server">
+    <div>
+    <p>Generation: <asp:Literal ID="litGeneration" runat="server" /></p>
+    <div class="code">
+    <asp:Literal ID="litDecoded" runat="server" />
+    </div>
+    </div>
+    </asp:PlaceHolder>
+</form>
+</asp:Content>
---
+++ gts/test/Decode.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+
+namespace PkmnFoundations.GTS.debug
+{
+    public partial class Decode : System.Web.UI.Page
+    {
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            litMessage.Text = "";
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnDecode_Click(object sender, EventArgs e)
+        {
+            byte[] data = null;
+            phDecoded.Visible = false;
+
+            try
+            {
+                data = GtsSession4.DecryptData(txtData.Text);
+                litGeneration.Text = "4";
+            }
+            catch (FormatException)
+            {
+            }
+
+            if (data == null) try
+            {
+                data = GtsSession5.DecryptData(txtData.Text);
+                litGeneration.Text = "5";
+            }
+            catch (FormatException)
+            {
+            }
+
+            if (data == null)
+            {
+                litMessage.Text = "<p class=\"errorMessage\">Data is not formatted correctly.</p>";
+                return;
+            }
+
+            litDecoded.Text = RenderHex(data.ToHexStringLower());
+            phDecoded.Visible = true;
+        }
+
+        private String RenderHex(String hex)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int x = 0; x < hex.Length; x += 8)
+            {
+                if (x % 16 == 0)
+                {
+                    builder.Append(x.ToString("x4"));
+                    builder.Append(": ");
+                }
+
+                builder.Append(hex.Substring(x, Math.Min(8, hex.Length - x)));
+                builder.Append((x % 16 == 0) ? " " : "<br />");
+            }
+            return builder.ToString();
+        }
+    }
+}
