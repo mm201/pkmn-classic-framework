404610c5d4972b7f53b262c3ab03e740e3ec0337
Greg Edwards <edwardsgreg@gmail.com>
2014-05-17 00:22:15 -0400

    Added battle video queueing page.

---
+++ gts/BattleVideo.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeBehind="BattleVideo.aspx.cs" Inherits="PkmnFoundations.GTS.BattleVideo" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="cpHead" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="cpMain" runat="server">
+<h1>Battle videos</h1>
+<p>Please enter your battle video's ID (eg. 12-34567-89012) into this form to have it backed up
+onto Foundations GTS. <strong>Only Generation IV is supported for now!</strong>:</p>
+<form id="theForm" runat="server">
+<div><asp:TextBox ID="txtBattleVideo" runat="server" />
+<asp:Button ID="btnSend" Text="Send" OnClick="btnSend_Click" runat="server" /></div>
+<asp:Literal ID="litMessage" runat="server" />
+</form>
+</asp:Content>
---
+++ gts/BattleVideo.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using PkmnFoundations.Data;
+
+namespace PkmnFoundations.GTS
+{
+    public partial class BattleVideo : System.Web.UI.Page
+    {
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            litMessage.Text = "";
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnSend_Click(object sender, EventArgs e)
+        {
+            ulong id;
+            UInt64.TryParse(txtBattleVideo.Text.Replace('-', ' ').Replace(" ", ""), out id);
+            if (id == 0)
+            {
+                litMessage.Text = "The battle video ID could not be read. Please enter a battle video ID in the format, xx-xxxxx-xxxxx, and try again.";
+                return;
+            }
+
+            using (MySqlConnection db = CreateConnection())
+            {
+                db.Open();
+                QueueVideoId(db, id);
+                db.Close();
+            }
+        }
+
+        private void QueueVideoId(MySqlConnection db, ulong id)
+        {
+            using (MySqlTransaction tran = db.BeginTransaction())
+            {
+                long count = (long)tran.ExecuteScalar("SELECT Count(*) FROM BattleVideoCrawlQueue WHERE SerialNumber = @serial_number", new MySqlParameter("@serial_number", id));
+                if (count > 0)
+                {
+                    tran.Rollback();
+                    litMessage.Text = "The battle video is already queued for retrieval.";
+                    return;
+                }
+                tran.ExecuteNonQuery("INSERT INTO BattleVideoCrawlQueue (SerialNumber, `Timestamp`) VALUES (@serial_number, NOW())", new MySqlParameter("@serial_number", id));
+                tran.Commit();
+                litMessage.Text = "The battle video has been queued for retrieval.";
+            }
+        }
+
+        public static MySqlConnection CreateConnection()
+        {
+            return new MySqlConnection(ConfigurationManager.ConnectionStrings["pkmnFoundationsConnectionString"].ConnectionString);
+        }
+    }
+}
