a6d7cec2914eddf0e29fae6d98e5f65e2126bb14
Greg Edwards <edwardsgreg@gmail.com>
2014-07-09 19:37:05 -0400

    Added project to insert GenV battle videos into the database.

--- bvRestorer5/Program.cs
+++ bvRestorer5/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.IO;
-using PkmnFoundations.Support;
-using PkmnFoundations.Structures;
+using System.Threading.Tasks;
 using PkmnFoundations.Data;
+using PkmnFoundations.Structures;
+using PkmnFoundations.Support;
 
-namespace bvRestorer4
+namespace bvRestorer5
 {
     class Program
     {
@@ -15,15 +16,10 @@ namespace bvRestorer4
         {
             if (args.Length == 0)
             {
-                Console.WriteLine("Usage: bvRestorer4 <path>");
+                Console.WriteLine("Usage: bvRestorer5 <path>");
                 Console.WriteLine("Attempts to insert all the files in path\ninto the database in app configuration.");
                 return;
             }
-
-            m_pad = new byte[256];
-            FileStream s = File.OpenRead(AppDomain.CurrentDomain.BaseDirectory + Path.DirectorySeparatorChar + "pad.bin");
-            s.Read(m_pad, 0, m_pad.Length);
-            s.Close();
 
             String[] filenames = Directory.GetFiles(args[0]);
             int successCount = 0;
@@ -31,32 +27,30 @@ namespace bvRestorer4
             foreach (String filename in filenames)
             {
                 FileStream fs = File.OpenRead(filename);
-                if (fs.Length != 0x1d60)
+                if (fs.Length != 0x18b8)
                 {
                     Console.WriteLine("{0}: file size is wrong, skipped.", filename);
                     continue;
                 }
 
-                byte[] data = new byte[0x1d60];
-                fs.ReadBlock(data, 0, 0x1d60);
+                byte[] data = new byte[0x18b8];
+                fs.ReadBlock(data, 0, 0x18b8);
                 fs.Close();
 
                 int length = BitConverter.ToInt32(data, 0);
-                if (length != 0x1d60)
+                if (length != 0x18b8)
                 {
                     Console.WriteLine("{0}: size field is wrong, skipped.", filename);
                     continue;
                 }
 
-                if (data[4] != 0xda)
+                if (data[4] != 0xf2)
                 {
                     Console.WriteLine("{0}: request type is wrong, skipped.", filename);
                     continue;
                 }
 
-                CryptMessage(data);
-
-                if (data[5] != 0x59 || data[6] != 0x00 || data[7] != 0x00)
+                if (data[5] != 0x55 || data[6] != 0x00 || data[7] != 0x00)
                 {
                     Console.WriteLine("{0}: sanity bytes are wrong, skipped.", filename);
                     continue;
@@ -64,30 +58,18 @@ namespace bvRestorer4
 
                 int pid = BitConverter.ToInt32(data, 0x08);
                 long serial = BitConverter.ToInt64(data, 0x0c);
-                byte[] mainData = new byte[0x1d4c];
-                Array.Copy(data, 0x14, mainData, 0, 0x1d4c);
+                byte[] mainData = new byte[0x18a4];
+                Array.Copy(data, 0x14, mainData, 0, 0x18a4);
 
-                BattleVideoRecord4 record = new BattleVideoRecord4(pid, serial, mainData);
+                BattleVideoRecord5 record = new BattleVideoRecord5(pid, serial, mainData);
 
-                DataAbstract.Instance.BattleVideoUpload4(record);
+                DataAbstract.Instance.BattleVideoUpload5(record);
 
                 Console.WriteLine("Video {0} added successfully.", BattleVideoHeader4.FormatSerial(serial));
                 successCount++;
             }
             Console.WriteLine("{0} battle videos successfully added.", successCount);
             Console.ReadKey();
-        }
-
-        private static byte[] m_pad;
-
-        private static void CryptMessage(byte[] message)
-        {
-            if (message.Length < 5) return;
-            byte padOffset = (byte)(message[0] + message[4]);
-
-            // encrypt and decrypt are the same operation...
-            for (int x = 5; x < message.Length; x++)
-                message[x] ^= m_pad[(x + padOffset) & 0xff];
         }
     }
 }
