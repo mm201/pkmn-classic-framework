eb6737bc9cfc5674de9fff59b2fc6ca7b79ebeef
Greg Edwards <edwardsgreg@gmail.com>
2014-08-05 03:07:08 -0400

    Some early trendy phrase work.

---
+++ library/Support/TrendyPhrase4.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PkmnFoundations.Support
+{
+    public class TrendyPhrase4
+    {
+        // todo: For a rainy day
+
+        public static String[,] PHRASES = new String[,]
+        {
+            {
+                "Please!\n{0}!",
+                "Go! {0}!",
+                "I’ll battle with\n{0}!",
+                "It’s {0}!",
+                "{0}, I’m going\nwith {1}!",
+                "Look at {0}!",
+                "I’ll show you {0}!",
+                "Now!\n{0}!",
+                "I’ll show you my\n{0} strategy!",
+                "I’ll {0}!",
+                "I’ll shock you with\n{0}!",
+                "This is the beginning\nof {0}!",
+                "This battle is\n{0}!",
+                "I don’t think I’ll\never lose at {0}!",
+                "Team {0} is here!",
+                "You think you can beat\n{0}?",
+                "{0}!\n{1} power!",
+                "This is the {0}\nPokémon!",
+                "{0} won’t lose!",
+                "Please {0}!\n{1}!"
+            },
+            {
+                "I win!\n{0}!",
+                "I won!\nI won with {0}!",
+                "{0} is strong,\nisn’t it?",
+                "It’s {0}\n{1} after all!",
+                "{0}, yay!",
+                "Yay, {0}!\n{1}!",
+                "Sorry, it’s {0}\n{1}.",
+                "{0}!\nThank you!",
+                "The way I feel now is\n{0}!",
+                "I wanted people to look at\nmy {0}!",
+                "It’s all thanks to\n{0}.",
+                "I might have won with\n{0}!",
+                "I get the happiest with\n{0}!",
+                "{0} secured\nthe victory!",
+                "This {0}\nwas really good!",
+                "{0}\nwas fun, wasn’t it?",
+                "Huh?\n{0}?!",
+                "{0} is the toughest!",
+                "Happy!\n{0} happy!",
+                "How’s that?!\n{0}!"
+            },
+            {
+                "You win...\n{0}",
+                "{0} was the one\nthing I wanted to avoid...",
+                "Waaah!\n{0}!",
+                "I want to go home with\n{0}...",
+                "{0}!\n{1}!",
+                "Could it be...?\n{0}...?",
+                "{0}!\nHow awful!",
+                "I was confident about\n{0}, too.",
+                "You're {0},\naren’t you?",
+                "{0}!\nCan’t be anything else but.",
+                "I feel so helplessly angry...\nIt’s {0}!",
+                "{0} makes me sad...",
+                "I feel sorry for\n{0}!",
+                "The way I feel now is\n{0}...",
+                "I lost, but I won at\n{0}!",
+                "I would’ve won if this\nwere {0}...",
+                "My head’s filled with only\n{0} now!",
+                "The way I lost...\nIt’s like {0}...",
+                "Isn’t {0}\n{1}?",
+                "Aww... That’s really\n{0}..."
+            },
+            {
+                "Hello!\n{0}!",
+                "I love {0}!",
+                "I love {0}!\nI love {1}, too!",
+                "This {0} is\n{1}, isn’t it?",
+                "I can do anything for\n{0}!",
+                "This {0} is\n{1}!",
+                "{0} is the real\n{1}!",
+                "It might be {0}...",
+                "There’s only {0}\nleft!",
+                "It’s {0}!\nIt’s {1}!",
+                "I prefer {0}\nafter all!",
+                "Is {0}\n{1}?",
+                "Do you like {0}?",
+                "What do you think of\n{0}?",
+                "{0} is so\n{1}!",
+                "{0} are\n{1}!",
+                "{0}, right?",
+                "Did you know {0}?\nIt’s {1}!",
+                "Excuse me...\nIt’s {0}!",
+                "{0}, right?\n{1}!"
+            },
+            {
+                "{0}!\nHello!",
+                "Glad to meet you!\nI love {0}!",
+                "I’m a {0} Trainer!\nPlease battle me!",
+                "Please trade!\nI’m offering {0}!",
+                "Please trade!\nI want a {0}!",
+                "I’ve entered the Union Room.",
+                "Let’s draw! I want to draw\n{0}!",
+                "I’ve got to go!\n{0}!",
+                "Please leave me alone...",
+                "Anyone want to\n{0}?",
+                "Let’s {0}!",
+                "Want to {0}?",
+                "I want to {0}!",
+                "OK!",
+                "I don’t want to\n{0}.",
+                "I’ll go wait at the Colosseum\nnow.",
+                "Please talk to me!",
+                "Do you know where I am?",
+                "I want to trade my {0}.\nPlease talk to me.",
+                "I want a {0} battle!\nPlease talk to me!"
+            }
+        };
+
+    }
+}
