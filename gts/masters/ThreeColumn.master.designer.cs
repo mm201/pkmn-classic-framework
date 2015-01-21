66dba6c1b1fd634e59699042dff80c0c75128c33
Greg Edwards <edwardsgreg@gmail.com>
2015-01-21 11:53:33 -0500

    Removed placeholders on ThreeColumn.master

--- gts/masters/ThreeColumn.master.designer.cs
+++ gts/masters/ThreeColumn.master.designer.cs
@@ -22,6 +22,15 @@ namespace PkmnFoundations.GTS {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder cpHead;
         
         /// <summary>
+        /// cpLeft control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder cpLeft;
+        
+        /// <summary>
         /// cpMain control.
         /// </summary>
         /// <remarks>
@@ -29,5 +38,14 @@ namespace PkmnFoundations.GTS {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder cpMain;
+        
+        /// <summary>
+        /// cpRight control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder cpRight;
     }
 }
