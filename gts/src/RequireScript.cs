f1be6c6e306efca538d11cb472132b3d02cc570a
Greg Edwards <edwardsgreg@gmail.com>
2015-01-21 11:27:41 -0500

    Added RequireScript control, use on master.

---
+++ gts/src/RequireScript.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PkmnFoundations.Web
+{
+    public class RequireScript : RequireLinkBase
+    {
+        public RequireScript() : base()
+        {
+
+        }
+
+        public override void RenderHeader(System.Web.UI.HtmlTextWriter writer)
+        {
+            writer.AddAttribute("src", ResolveUrl(ScriptUrl ?? ""));
+            writer.AddAttribute("type", Type ?? "text/javascript");
+            writer.RenderBeginTag("script");
+            writer.RenderEndTag();
+        }
+
+        public String ScriptUrl { get; set; }
+        public String Type { get; set; }
+    }
+}
