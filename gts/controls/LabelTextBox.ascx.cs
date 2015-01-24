5f611534bf9f4f10d44eaccece7da45aa63a7bb4
Greg Edwards <edwardsgreg@gmail.com>
2015-01-23 21:13:53 -0500

    Added LabelTextBox control which renders a textbox with a label contained inside.

---
+++ gts/controls/LabelTextBox.ascx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PkmnFoundations.GTS.controls
+{
+    public partial class LabelTextBox : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public String Text
+        {
+            get
+            {
+                return theTextBox.Text;
+            }
+            set
+            {
+                theTextBox.Text = value;
+            }
+        }
+
+        public String Label
+        {
+            get
+            {
+                return theLabel.Text;
+            }
+            set
+            {
+                theLabel.Text = value;
+            }
+        }
+
+        public TextBoxMode TextMode
+        {
+            get
+            {
+                return theTextBox.TextMode;
+            }
+            set
+            {
+                theTextBox.TextMode = value;
+            }
+        }
+    }
+}
---
+++ gts/script/form.js
@@ -0,0 +1,13 @@
+﻿function labelTextBox_Change()
+{
+    if (this.value.length > 0)
+        $(this).addClass("hasContent");
+    else
+        $(this).removeClass("hasContent");
+}
+
+$(document).ready(function ()
+{
+    $("input").change(labelTextBox_Change);
+    $("input").each(labelTextBox_Change);
+})
