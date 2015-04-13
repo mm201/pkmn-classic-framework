5e714435a165c285d3e59dd1d2ad6735296eb57f
Greg Edwards <edwardsgreg@gmail.com>
2015-04-13 19:48:36 -0400

    Added formatter classes

---
+++ library/Structures/Format.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PkmnFoundations.Structures
+{
+    public static class Format
+    {
+        private static String[] m_marks = new String[] { "●", "▲", "■", "♥", "★", "♦" };
+
+        public static String Markings(Markings markings, String trueFormat, String falseFormat, String delimiter)
+        {
+            StringBuilder result = new StringBuilder();
+            int marking = 1;
+            for (int value = 0; value < 6; value++)
+            {
+                if (marking > 1) result.Append(delimiter);
+                String format = (((int)markings & marking) != 0) ? trueFormat : falseFormat;
+                result.Append(String.Format(format, m_marks[value]));
+
+                marking <<= 1;
+            }
+
+            return result.ToString();
+        }
+
+        public static String GenderSymbol(Genders gender)
+        {
+            switch (gender)
+            {
+                case Genders.Male:
+                    return "♂";
+                case Genders.Female:
+                    return "♀";
+                default:
+                    return "";
+            }
+        }
+    }
+}
---
+++ web/src/WebFormat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using PkmnFoundations.Structures;
+
+namespace PkmnFoundations.Web
+{
+    public static class WebFormat
+    {
+        public static String Markings(Markings markings)
+        {
+            return Format.Markings(markings, "<span class=\"m\">{0}</span>", "<span>{0}</span>", "");
+        }
+
+        public static String PokemonImage(PokemonPartyBase pokemon)
+        {
+            return (pokemon.IsShiny ? "~/images/pkmn-lg-s/" : "~/images/pkmn-lg/") +
+                PokemonImage2(pokemon) + ".png";
+        }
+
+        private static String PokemonImage2(PokemonPartyBase pkmn)
+        {
+            // todo: spinda
+            StringBuilder builder = new StringBuilder();
+            builder.Append(pkmn.SpeciesID);
+            if (pkmn.Form.Suffix.Length > 0)
+            {
+                builder.Append('-');
+                builder.Append(pkmn.Form.Suffix);
+            }
+            if (pkmn.Species.GenderVariations && pkmn.Gender == Genders.Female)
+                builder.Append("-f");
+
+            return builder.ToString();
+        }
+    }
+}
