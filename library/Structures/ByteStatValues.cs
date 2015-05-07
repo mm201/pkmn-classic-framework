fc0c6bfd48396b2724e014068c9b6d49798266d7
Greg Edwards <edwardsgreg@gmail.com>
2015-05-07 01:19:49 -0400

    Fixed speed getting overwritten with special attack when parsing stats.

--- library/Structures/ByteStatValues.cs
+++ library/Structures/ByteStatValues.cs
@@ -8,7 +8,7 @@ namespace PkmnFoundations.Structures
     public class ByteStatValues : StatValues<byte>
     {
         public ByteStatValues(byte hp, byte attack, byte defense, byte speed, byte special_attack, byte special_defense)
-            : base(hp, attack, defense, special_attack, special_attack, special_defense)
+            : base(hp, attack, defense, speed, special_attack, special_defense)
         {
         }
 
--- library/Structures/IntStatValues.cs
+++ library/Structures/IntStatValues.cs
@@ -8,7 +8,7 @@ namespace PkmnFoundations.Structures
     public class IntStatValues : StatValues<int>
     {
         public IntStatValues(int hp, int attack, int defense, int speed, int special_attack, int special_defense)
-            : base(hp, attack, defense, special_attack, special_attack, special_defense)
+            : base(hp, attack, defense, speed, special_attack, special_defense)
         {
         }
 
